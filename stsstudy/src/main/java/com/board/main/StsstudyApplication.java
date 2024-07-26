7bb2c9a84630ca1df7adb65fd506ae0ab4a416c5
USER <USER@DESKTOP-UKG20GI>
2024-07-26 21:56:37 +0900

    git 연결

---
+++ stsstudy/src/main/java/com/board/main/StsstudyApplication.java
@@ -0,0 +1,13 @@
+package com.board.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsstudyApplication.class, args);
+	}
+
+}
